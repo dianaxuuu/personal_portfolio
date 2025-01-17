796d08de27a84179975a825e907689138430c327
dianaxuuu <dianaxush@hotmail.com>
2025-01-17 12:24:21 -0500

    update some text and layout in about and projects sections.

--- my-app/app/components/Projects.jsx
+++ my-app/app/components/Projects.jsx
@@ -9,33 +9,35 @@ const Projects = () => {
 
             <h4
                 className='text-center mb-2 text-lg font-Ovo'>
-                My portfolio</h4>
+                What I did</h4>
 
             <h2
                 className='text-center text-5xl font-Ovo'>
-                My latest projects</h2>
+                My projects</h2>
 
             {/* <p
                 className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
                 Welcome to my  portfolio! Here is a collection of projects showcasing my expertise.</p> */}
 
-            <div
-                className='grid grid-cols-auto my-10 gap-5'>
+            <div className="grid grid-cols-auto my-10 gap-5">
                 {projectData.map((project, index) => (
-                    <div
-                        key={index}
-                        className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
-                        style={{ backgroundImage: `url(${project.bgImage})` }}>
-                        <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
+                    <div key={index} className="rounded-lg cursor-pointer group">
+                        {/* Background Image */}
+                        <div
+                            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg"
+                            style={{ backgroundImage: `url(${project.bgImage})` }}
+                        ></div>
+
+                        {/* Project Details */}
+                        <div className="bg-white w-full rounded-md mt-3 py-3 px-5 flex items-center justify-between group-hover:-translate-y-2 transition-transform duration-300">
                             <div>
-                                <h2 className='font-semibold'>{project.title}</h2>
-                                <p className='text-sm text-gray-700'>{project.description}</p>
+                                <h2 className="font-semibold">{project.title}</h2>
+                                <p className="text-sm text-gray-700">{project.description}</p>
                             </div>
-                            <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
-                                <Image src={assets.send_icon} alt='send icon' className='w-5' />
+                            <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                                <Image src={assets.send_icon} alt="send icon" className="w-5" />
                             </div>
                         </div>
-
                     </div>
                 ))}
             </div>
